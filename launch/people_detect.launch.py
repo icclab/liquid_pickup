5229018dadf4648510cc47911495501fc3312202
Gaurav Kapoor <gkapoordelhi@gmail.com>
2025-04-03 14:23:02 +0000

    added people detect arm pose

---
+++ launch/people_detect.launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+
+    coordinates = LaunchConfiguration('coordinates')    
+
+    coordinates_launch_arg = DeclareLaunchArgument(
+        'coordinates',
+        default_value='"[[0.5, 0], [0.8, 0], [1.0, 0.0]]"'
+    )
+
+    return LaunchDescription([
+
+        coordinates_launch_arg,
+
+        Node(
+            package='liquid_pickup',
+            executable='liquid_pickup_node',
+            name='bt_node',
+            namespace='summit',
+            parameters=[{"bt_xml": "people_detect.xml"}, {"use_sim_time": False}, {"coordinates": coordinates}],
+            output='screen',
+            remappings=[('/tf', '/summit/tf'), ('/tf_static', '/summit/tf_static')],
+            emulate_tty=True,
+        ),
+    ])
